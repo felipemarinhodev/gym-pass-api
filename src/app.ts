a15a991b8ea7e7cea5ce583be3bc21e1d1903ce8
felipemarinhodev <felipmarinho@gmail.com>
2023-03-09 18:12:03 -0800

    chore: install prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: { name: 'Felipe Marinho', email: 'felipe@felipemarinho.dev' },
+})
