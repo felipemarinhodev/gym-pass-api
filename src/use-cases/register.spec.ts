f453381f20d4bb4b368740ab7a608ec18b871f8a
felipemarinhodev <felipmarinho@gmail.com>
2023-03-10 13:13:10 -0800

    test: create an InMemoryUserRepository

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,23 +1,11 @@
+import { InMemoryUsersRepository } from './../repositories/in-memory/in-memory-users-repository'
 import { compare } from 'bcryptjs'
 import { describe, expect, it } from 'vitest'
 import { RegisterUseCase } from './register'
 
 describe('Register Use Case', () => {
   it('should hash user password upon registration', async () => {
-    const registerUseCase = new RegisterUseCase({
-      async findByEmail(email) {
-        return null
-      },
-      async create(data) {
-        return {
-          id: 'user-1',
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        }
-      },
-    })
+    const registerUseCase = new RegisterUseCase(new InMemoryUsersRepository())
 
     const { user } = await registerUseCase.execute({
       name: 'Fulano',
