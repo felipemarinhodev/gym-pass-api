a01516a556fc3aa6bf0eb906b04393d29b98206d
felipemarinhodev <felipmarinho@gmail.com>
2023-03-13 20:22:30 -0700

    test: ensure not be able to authenticate with wrong email

--- src/use-cases/authenticate-user.spec.ts
+++ src/use-cases/authenticate-user.spec.ts
@@ -4,24 +4,28 @@ import { describe, expect, it, vi } from 'vitest'
 import { InMemoryUsersRepository } from './../repositories/in-memory/in-memory-users-repository'
 import { AuthenticateUseCase } from './authenticate'
 
+const PASSWORD = '123456'
+
+async function makeSut() {
+  const usersRepository = new InMemoryUsersRepository()
+  const sut = new AuthenticateUseCase(usersRepository)
+
+  const currentUser = {
+    id: 'user-1',
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    password_hash: await hash(PASSWORD, 6),
+    created_at: new Date(),
+  }
+
+  return { usersRepository, sut, currentUser }
+}
+
 describe('Authenticate Use Case', () => {
   it('should be able to authenticate', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateUseCase(usersRepository)
-
-    const currentUser = {
-      id: 'user-1',
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password_hash: await hash('123456', 6),
-      created_at: new Date(),
-    }
-    vi.spyOn(usersRepository, 'create').mockReturnValueOnce(
-      new Promise((resolve, reject) => resolve(currentUser)),
-    )
-
+    const { usersRepository, sut, currentUser } = await makeSut()
+    vi.spyOn(usersRepository, 'create').mockResolvedValue(currentUser)
     vi.spyOn(usersRepository, 'findByEmail').mockResolvedValue(currentUser)
-
     const { user } = await sut.execute({
       email: 'johndoe@example.com',
       password: '123456',
@@ -29,16 +33,7 @@ describe('Authenticate Use Case', () => {
     expect(user.id).toEqual(expect.any(String))
   })
   it('should not be able to authenticate with wrong email', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateUseCase(usersRepository)
-
-    const currentUser = {
-      id: 'user-1',
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password_hash: await hash('123456', 6),
-      created_at: new Date(),
-    }
+    const { usersRepository, sut, currentUser } = await makeSut()
     vi.spyOn(usersRepository, 'create').mockResolvedValue(currentUser)
 
     vi.spyOn(usersRepository, 'findByEmail').mockReturnValueOnce(
