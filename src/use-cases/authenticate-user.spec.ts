20d325e7680fbda176b7bad762d79d4fec0ec56a
felipemarinhodev <felipmarinho@gmail.com>
2023-03-13 20:13:12 -0700

    test: ensure not be able to authenticate with wrong email

--- src/use-cases/authenticate-user.spec.ts
+++ src/use-cases/authenticate-user.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 import { hash } from 'bcryptjs'
 import { describe, expect, it, vi } from 'vitest'
 import { InMemoryUsersRepository } from './../repositories/in-memory/in-memory-users-repository'
@@ -26,5 +27,29 @@ describe('Authenticate Use Case', () => {
       password: '123456',
     })
     expect(user.id).toEqual(expect.any(String))
+  })
+  it('should not be able to authenticate with wrong email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    const currentUser = {
+      id: 'user-1',
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+      created_at: new Date(),
+    }
+    vi.spyOn(usersRepository, 'create').mockResolvedValue(currentUser)
+
+    vi.spyOn(usersRepository, 'findByEmail').mockReturnValueOnce(
+      new Promise((resolve, reject) => reject(new InvalidCredentialsError())),
+    )
+
+    expect(() =>
+      sut.execute({
+        email: 'fulano@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })
