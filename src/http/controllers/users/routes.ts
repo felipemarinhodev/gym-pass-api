040c1902ad69bc7f85862118733ba098f50e2776
felipemarinhodev <felipmarinho@gmail.com>
2023-04-26 23:03:40 -0700

    feat: :sparkles: generate the refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { register } from './register'
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/session', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
